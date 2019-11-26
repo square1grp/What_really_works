6b04853070354bcf26e2bfb201d76e0a6c24e4c8
square1grp <developer01@square1grp.com>
2019-11-26 01:39:12 -0800

    add validations

--- wrw/static/js/add_treatment.js
+++ wrw/static/js/add_treatment.js
@@ -2,10 +2,16 @@ $(document).ready(function () {
     console.log($("#ended_treatment"))
     $("#ended_treatment").click(function () {
         if ($(this).prop("checked")) {
+            $("#end_section .form-control").prop("required", true);
             $("#end_section").show();
+
+            $("#start_severity_on_end_date .form-control").prop("required", true);
             $("#start_severity_on_end_date").show();
         } else {
+            $("#end_section .form-control").prop("required", false);
             $("#end_section").hide();
+
+            $("#start_severity_on_end_date .form-control").prop("required", false);
             $("#start_severity_on_end_date").hide();
         }
     });
